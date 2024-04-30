5cc9f486fef09cb61aa8e25585e98625c026244d
Alvaromn95 <amennav382@g.educaand.es>
2024-04-30 14:12:59 +0200

    ultimo

--- main.java
+++ main.java
@@ -3,7 +3,5 @@ public class main {
     public static void main(String[] args) {
         Juego juego1 = new Juego();
         juego1.partida();
-        Juego juego2 = new Juego();
-        juego2.partida();
     }
 }
