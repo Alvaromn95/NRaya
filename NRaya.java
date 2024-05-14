cfd2c7fa2342382ceeade39a5327323f29e3c3ef
Alvaromn95 <amennav382@g.educaand.es>
2024-05-14 15:42:04 +0200

    Update NRaya.java

--- NRaya.java
+++ NRaya.java
@@ -1,21 +1,21 @@
 import java.util.Scanner;
 
 public class NRaya extends Juegos {
-    private int empate = 0;
-    protected char tablero[][];
+     int empate = 0;
+     char tablero[][];
     String nom1;
     String nom2;
     char ficha1;
     char ficha2;
-    protected boolean turno1;
-    protected boolean turno2;
+    boolean turno1;
+    boolean turno2;
     boolean terminarJuego = false;
-    
-    public NRaya(int tamTab){
+
+    public NRaya(int tamTab) {
         tablero = new char[tamTab][tamTab];
     }
 
-    public void rellenar() {
+    public void rellenarTablero() {
         for (int i = 0; i < tablero.length; i++) {
             for (int j = 0; j < tablero.length; j++) {
                 if ((tablero[i][j] == ficha1) || (tablero[i][j] == ficha2)) {
@@ -28,8 +28,49 @@ public class NRaya extends Juegos {
         }
         System.out.println();
     }
+    public boolean CambiarTurno() {
+        if (turno1 == false) {
+            turno1 = true;
+            turno2 = false;
+        } else {
+            turno1 = false;
+            turno2 = true;
+        }
+        return turno1;
+    }
 
-    public void introducirFichas() { 
+
+    public boolean quienEmpieza() {
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Jugador 1,introduzca su nombre");
+        nom1 = leer.next();
+        System.out.println("Elige su ficha");
+        ficha1 = leer.next().charAt(0);
+        System.out.println("El jugador 1 es " + nom1 + " y su ficha es " + ficha1);
+        System.out.println("Jugador 2,introduzca su nombre");
+        nom2 = leer.next();
+        System.out.println("Elige su ficha");
+        ficha2 = leer.next().charAt(0);
+        System.out.println("El jugador 1 es " + nom2 + " y su ficha es " + ficha2);
+        System.out.println("Quien quiere comenzar,el 1 o el 2?");
+        int num = leer.nextInt();
+        if (num > 2) {
+            System.out.println("Error, introduce un numero entre 1 y 2");
+            quienEmpieza();
+        } else {
+            if (num == 1) {
+                turno1 = true;
+                turno2 = false;
+                return turno1;
+            } else {
+                turno2 = true;
+                turno1 = false;
+                return turno2;
+            }
+        }
+        return false;
+    }
+    public void introducirFichas() {
         Scanner leer = new Scanner(System.in);
         int fila = 0, columna = 0;
         if (turno1 == true) {
@@ -53,76 +94,7 @@ public class NRaya extends Juegos {
             empate++;
         }
     }
-
-    public boolean ElegirComiezo() {
-        Scanner leer = new Scanner(System.in);
-        System.out.println("Jugador 1,introduzca su nombre");
-
-        nom1 = leer.next();
-
-        System.out.println("Indique cual quiere que sea su ficha");
-        ficha1 = leer.next().charAt(0);
-        System.out.println("El jugador 1 es " + nom1 + " y su ficha es " + ficha1);
-        System.out.println("Jugador 2,introduzca su nombre");
-
-        nom2 = leer.next();
-
-        System.out.println("Indique cual quiere que sea su ficha");
-        ficha2 = leer.next().charAt(0);
-        System.out.println("El jugador 1 es " + nom2 + " y su ficha es " + ficha2);
-        System.out.println("Quien quiere comenzar,el 1 o el 2?");
-        int num = leer.nextInt();
-        if (num > 2) {
-            System.out.println("Error, introduce un numero entre 1 y 2");
-            ElegirComiezo();
-        } else {
-            if (num == 1) {
-                turno1 = true;
-                turno2 = false;
-                return turno1;
-            } else {
-                turno2 = true;
-                turno1 = false;
-                return turno2;
-            }
-        }
-        return false;
-    }
-
-    public boolean CambiarTurno() {
-        if (turno1 == false) {
-            turno1 = true;
-            turno2 = false;
-        } else {
-            turno1 = false;
-            turno2 = true;
-        }
-        return turno1;
-    }
-
     
-
-    public boolean ComprobarFilas() {
-        for (int i = 0; i < tablero.length; i++) {
-            int contFicha1 = 0;
-            int contFicha2 = 0;
-            for (int j = 0; j < tablero.length; j++) {
-                if (tablero[i][j] == ficha1) {
-                    contFicha1++;
-                } else if (tablero[i][j] == ficha2) {
-                    contFicha2++;
-                }
-            }
-            if (contFicha2 == tablero.length) {
-                System.out.println(nom2 + "ha ganado");
-                return true;
-            } else if (contFicha1 == tablero.length) {
-                System.out.println(nom1 + " ha ganado");
-                return true;
-            }
-        }
-        return false;
-    }
 
     public boolean ComprobarCol() {
         for (int i = 0; i < tablero.length; i++) {
@@ -137,6 +109,28 @@ public class NRaya extends Juegos {
             }
             if (contFicha2 == tablero.length) {
                 System.out.println(nom2 + " ha ganado");
+                return true;
+            } else if (contFicha1 == tablero.length) {
+                System.out.println(nom1 + " ha ganado");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean ComprobarFilas() {
+        for (int i = 0; i < tablero.length; i++) {
+            int contFicha1 = 0;
+            int contFicha2 = 0;
+            for (int j = 0; j < tablero.length; j++) {
+                if (tablero[i][j] == ficha1) {
+                    contFicha1++;
+                } else if (tablero[i][j] == ficha2) {
+                    contFicha2++;
+                }
+            }
+            if (contFicha2 == tablero.length) {
+                System.out.println(nom2 + "ha ganado");
                 return true;
             } else if (contFicha1 == tablero.length) {
                 System.out.println(nom1 + " ha ganado");
@@ -175,6 +169,7 @@ public class NRaya extends Juegos {
 
         return false;
     }
+
     public boolean Empate() {
         if (empate == tablero.length * tablero.length) {
             System.out.println("Empate");
@@ -184,28 +179,30 @@ public class NRaya extends Juegos {
     }
 
     int cont = 1;
+
     public void jugar() {
-        
-        
-        if(cont == 0) {
-            ElegirComiezo();
+
+        if (cont == 0) {
+            quienEmpieza();
             cont++;
-       
-            }
-            rellenar();
-            introducirFichas();
-            CambiarTurno();
-            terminarJuego();
-}
-public boolean cambioJuego(){
-    return terminarJuego;
-}
-public boolean terminarJuego() {
-    if (ComprobarCol() == true || ComprobarFilas() == true || Empate() == true || comprobarDiagonales() == true) {
-        terminarJuego=true;
-        return true;
+
+        }
+        rellenarTablero();
+        introducirFichas();
+        CambiarTurno();
+        terminarJuego();
     }
-    terminarJuego=false;
-    return false;
-}
+
+    public boolean cambioJuego() {
+        return terminarJuego;
+    }
+
+    public boolean terminarJuego() {
+        if (ComprobarCol() == true || ComprobarFilas() == true || Empate() == true || comprobarDiagonales() == true) {
+            terminarJuego = true;
+            return true;
+        }
+        terminarJuego = false;
+        return false;
+    }
 }
